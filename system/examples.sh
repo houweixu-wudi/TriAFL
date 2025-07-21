0a66ec18fc6729e79f2cdc318af7d9540201a3ee
houweixu-wudi <74530755+houweixu-wudi@users.noreply.github.com>
2025-07-21 23:24:23 +0800

    Add files via upload

---
+++ system/examples.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# python main.py -data Cifar10_dir10 -mn resnet18 -algo FedRT -gr 10000 -nb 10 -nc 20 -lbs 64 -ww 1 -locs 1 -eg 20
+
+
---
+++ system/utils/data_utils.py
@@ -0,0 +1,123 @@
+import numpy as np
+import os
+import torch
+import torchvision.transforms as transforms
+
+def read_data(dataset, idx, is_train=True):
+    if is_train:
+        train_data_dir = os.path.join('../dataset', dataset, 'train/')
+        
+        train_file = train_data_dir + str(idx) + '.npz'
+        with open(train_file, 'rb') as f:
+            train_data = np.load(f, allow_pickle=True)['data'].tolist()
+
+        return train_data
+
+    else:
+        test_data_dir = os.path.join('../dataset', dataset, 'test/')
+
+        test_file = test_data_dir + str(idx) + '.npz'
+        with open(test_file, 'rb') as f:
+            test_data = np.load(f, allow_pickle=True)['data'].tolist()
+
+        return test_data
+
+
+def read_client_data(dataset, idx, is_train=True):
+    if dataset[:2] == "ag" or dataset[:2] == "SS":
+        return read_client_data_text(dataset, idx, is_train)
+    elif dataset[:2] == "sh":
+        return read_client_data_shakespeare(dataset, idx)
+
+    if is_train:
+        train_data = read_data(dataset, idx, is_train)
+        X_train = torch.Tensor(train_data['x']).type(torch.float32)
+        y_train = torch.Tensor(train_data['y']).type(torch.int64)
+
+        train_data = [(x, y) for x, y in zip(X_train, y_train)]
+        return train_data
+    else:
+        test_data = read_data(dataset, idx, is_train)
+        X_test = torch.Tensor(test_data['x']).type(torch.float32)
+        y_test = torch.Tensor(test_data['y']).type(torch.int64)
+        test_data = [(x, y) for x, y in zip(X_test, y_test)]
+        return test_data
+
+
+def read_client_data_text(dataset, idx, is_train=True):
+    if is_train:
+        train_data = read_data(dataset, idx, is_train)
+        X_train, X_train_lens = list(zip(*train_data['x']))
+        y_train = train_data['y']
+
+        X_train = torch.Tensor(X_train).type(torch.int64)
+        X_train_lens = torch.Tensor(X_train_lens).type(torch.int64)
+        y_train = torch.Tensor(train_data['y']).type(torch.int64)
+
+        train_data = [((x, lens), y) for x, lens, y in zip(X_train, X_train_lens, y_train)]
+        return train_data
+    else:
+        test_data = read_data(dataset, idx, is_train)
+        X_test, X_test_lens = list(zip(*test_data['x']))
+        y_test = test_data['y']
+
+        X_test = torch.Tensor(X_test).type(torch.int64)
+        X_test_lens = torch.Tensor(X_test_lens).type(torch.int64)
+        y_test = torch.Tensor(test_data['y']).type(torch.int64)
+
+        test_data = [((x, lens), y) for x, lens, y in zip(X_test, X_test_lens, y_test)]
+        return test_data
+
+
+def read_client_data_shakespeare(dataset, idx, is_train=True):
+    if is_train:
+        train_data = read_data(dataset, idx, is_train)
+        X_train = torch.Tensor(train_data['x']).type(torch.int64)
+        y_train = torch.Tensor(train_data['y']).type(torch.int64)
+
+        train_data = [(x, y) for x, y in zip(X_train, y_train)]
+        return train_data
+    else:
+        test_data = read_data(dataset, idx, is_train)
+        X_test = torch.Tensor(test_data['x']).type(torch.int64)
+        y_test = torch.Tensor(test_data['y']).type(torch.int64)
+        test_data = [(x, y) for x, y in zip(X_test, y_test)]
+        return test_data
+
+
+class Dataset(torch.utils.data.Dataset):
+    
+    def __init__(self, data, train=True, dataset_name='Cifar10'):
+        self.name = dataset_name
+        self.data = data
+        if 'Cifar10' in self.name:
+            self.train = train
+            self.transform = transforms.Compose([transforms.ToTensor()])
+        # else:
+        #     raise NotImplementedError
+            
+           
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        if 'Cifar10' in self.name:
+            img = self.data[idx][0]
+            img = img.numpy()
+            if self.train:
+                img = np.flip(img, axis=2).copy() if (np.random.rand() > .5) else img # Horizontal flip
+                if (np.random.rand() > .5):
+                # Random cropping 
+                    pad = 4
+                    extended_img = np.zeros((3,32 + pad *2, 32 + pad *2)).astype(np.float32)
+                    extended_img[:,pad:-pad,pad:-pad] = img
+                    dim_1, dim_2 = np.random.randint(pad * 2 + 1, size=2)
+                    img = extended_img[:,dim_1:dim_1+32,dim_2:dim_2+32]
+            img = np.moveaxis(img, 0, -1) #重排通道顺序
+            img = self.transform(img) # NumPy数组转换为 PyTorch 的 Tensor 对象，默认会重新排列通道维度的顺序
+            
+            y = self.data[idx][1]
+            return img, y
+        else:
+            # 不采用数据增强，直接返回
+            return self.data[idx]
